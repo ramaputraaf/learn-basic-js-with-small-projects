16fe115ba341124a0aeba59f6bcdc310827b6f76
ramaputraaf <ramapaf122@gmail.com>
2023-05-23 23:06:56 +0700

    changing to 0.source-learn-javascript-main

--- 5. build-chrome-ext/2. when-to-use-let-and-const/index.js
+++ 5. build-chrome-ext/2. when-to-use-let-and-const/index.js
@@ -6,3 +6,23 @@ credits -= 10
 /* 
 1. penggunaan let/const selain untuk menetapkan sebuah variable, tapi juga digunakan untuk memberikan sinyal atau komunikasi kepada programmer lain bahwa variable tersebut dapat atau tidak dapat di ubah, jika variable tersebut di assign menggunakan let atau const
 */
+/* ========================================= */
+// If possible, use const. If not, use let.
+
+// Which variables below should be changed from let to const?
+
+// The customer wants to order some stuff. Here are the details:
+const basePrice = 520
+let discount = 120
+let shippingCost = 12
+let shippingTime = "5-12 days"
+
+// Whops! Turns out the shipping will be a bit more complex
+shippingCost = 15
+shippingTime = "7-14 days"
+
+// Calculating the full price
+let fullPrice = basePrice - discount + shippingCost
+
+// Finally, notifying the customer
+console.log("Total cost: " + fullPrice + ". It will arrive in " + shippingTime)
